6f86a9cefb3503e5009470e64ebe64d9658bbb61
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-10 22:47:34 +1000

    add login container component

--- libs/shared/auth/src/lib/auth.module.ts
+++ libs/shared/auth/src/lib/auth.module.ts
@@ -5,6 +5,7 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { authReducer, initialState as authInitialState } from './+state/auth.reducer';
 import { AuthEffects } from './+state/auth.effects';
+import { LoginComponent } from '../../containers/login/login.component';
 @NgModule({
   imports: [
     CommonModule,
@@ -17,6 +18,7 @@ import { AuthEffects } from './+state/auth.effects';
 
     EffectsModule.forFeature([AuthEffects])
   ],
-  providers: [AuthEffects]
+  providers: [AuthEffects],
+  declarations: [LoginComponent]
 })
 export class AuthModule {}
