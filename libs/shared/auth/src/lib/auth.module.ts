6106d9f5c9a8fea5b530a6d02febd277705269a0
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-10 22:53:52 +1000

    update route for login component

--- libs/shared/auth/src/lib/auth.module.ts
+++ libs/shared/auth/src/lib/auth.module.ts
@@ -3,18 +3,21 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-import { authReducer, initialState as authInitialState } from './+state/auth.reducer';
+import {
+  authReducer,
+  initialState as authInitialState
+} from './+state/auth.reducer';
 import { AuthEffects } from './+state/auth.effects';
 import { LoginComponent } from '../../containers/login/login.component';
 @NgModule({
   imports: [
     CommonModule,
 
-    RouterModule.forChild([
-      /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
-    ]),
+    RouterModule.forChild([{ path: '', component: LoginComponent }]),
 
-    StoreModule.forFeature('auth', authReducer, { initialState: authInitialState }),
+    StoreModule.forFeature('auth', authReducer, {
+      initialState: authInitialState
+    }),
 
     EffectsModule.forFeature([AuthEffects])
   ],
