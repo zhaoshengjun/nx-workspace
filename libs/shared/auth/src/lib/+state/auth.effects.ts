505874d7e8c3d2539f245d953a34c08ab9032499
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-12 22:38:27 +1000

    update auth effects

--- libs/shared/auth/src/lib/+state/auth.effects.ts
+++ libs/shared/auth/src/lib/+state/auth.effects.ts
@@ -1,30 +1,36 @@
+import { AuthService } from './../../services/auth/auth.service';
 import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
 import {
-  AuthActions,
   AuthActionTypes,
-  LoadAuth,
-  AuthLoaded
+  LoginAction,
+  LoginSuccessAction
 } from './auth.actions';
 import { AuthState } from './auth.reducer';
 import { DataPersistence } from '@nrwl/nx';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AuthEffects {
-  @Effect() effect$ = this.actions$.ofType(AuthActionTypes.AuthAction);
+  @Effect() effect$ = this.actions$.ofType(AuthActionTypes.Login);
 
   @Effect()
-  loadAuth$ = this.dataPersistence.fetch(AuthActionTypes.LoadAuth, {
-    run: (action: LoadAuth, state: AuthState) => {
-      return new AuthLoaded(state);
+  loadAuth$ = this.dataPersistence.fetch(AuthActionTypes.Login, {
+    run: (action: LoginAction, state: AuthState) => {
+      return this.authService.login().pipe(
+        map(user => {
+          return new LoginSuccessAction(user);
+        })
+      );
     },
 
-    onError: (action: LoadAuth, error) => {
+    onError: (action: LoginAction, error) => {
       console.error('Error', error);
     }
   });
 
   constructor(
+    private authService: AuthService,
     private actions$: Actions,
     private dataPersistence: DataPersistence<AuthState>
   ) {}
