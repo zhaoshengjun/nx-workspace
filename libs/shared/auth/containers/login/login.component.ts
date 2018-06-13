70b402162f9bb1f8978b0ad271a507aa4eba8c9e
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-13 22:09:55 +1000

    update login component

--- libs/shared/auth/containers/login/login.component.ts
+++ libs/shared/auth/containers/login/login.component.ts
@@ -1,4 +1,7 @@
+import { AuthState } from "./../../src/lib/+state/auth.reducer";
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { LoginAction } from "@nx-workspace/shared/auth/src/lib/+state/auth.actions";
 
 @Component({
   selector: 'nx-workspace-login',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store: Store<AuthState>) { }
 
   ngOnInit() {
   }
 
+  login() {
+    this.store.dispatch(new LoginAction())
+  }
 }
